ddf14be6f8e0abfe1434e8ec94e9ea9b2d7b5361
sbr69 <shubhadeepb68@gmail.com>
2026-04-02 15:57:15 +0530

    update UI/UX

--- Day4-10/myWeb/newApp/urls.py
+++ Day4-10/myWeb/newApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('app/', views.app, name='app'),
+    path('products/', views.app, name='products'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
